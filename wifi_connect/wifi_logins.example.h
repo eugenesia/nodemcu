02669242900fcd8821202746c9fbc529ff863be2
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-13 21:06:46 +0100

    Add code ref for SSID max len 32

--- wifi_connect/wifi_logins.example.h
+++ wifi_connect/wifi_logins.example.h
@@ -2,6 +2,10 @@
 
 // SSID 32 chars may be too long
 // http://forum.arduino.cc/index.php?topic=295728.0
+//
+// In ESP8266 code for Arduino
+// #define WL_SSID_MAX_LENGTH 32
+// https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/include/wl_definitions.h#L30
 
 struct WifiLogin {
   const char* ssid;
@@ -11,4 +15,3 @@ struct WifiLogin {
 WifiLogin hyperoptic = { "my-ssid-1", "my-password" };
 WifiLogin sky = { "my-ssid-2", "my-password" };
 WifiLogin lyv = { "my-ssid-3", "my-password" };
-

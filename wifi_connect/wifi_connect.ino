65aac30da04fdbabbcbe324ffcc256adf483e82e
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-13 21:45:02 +0100

    Convert Wifi status to string and print to serial

--- wifi_connect/wifi_connect.ino
+++ wifi_connect/wifi_connect.ino
@@ -2,7 +2,7 @@
 #include <ESP8266WiFi.h>
 #include "wifi_logins.h"
 
-WifiLogin wifi = lyv;
+WifiLogin wifi = hyperoptic;
 
 // Lolin NodeMCU uses GPIO2 for LED.
 // https://arduino.stackexchange.com/questions/38477/does-the-node-mcu-v3-lolin-not-have-a-builtin-led
@@ -19,10 +19,13 @@ void setup(void)
   Serial.begin(9600);
   // Connect to WiFi
   WiFi.begin(wifi.ssid, wifi.password);
+
+  char *statusStr;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
-    Serial.print(WiFi.status());
+    Serial.print('\n');
+    statusStr = wifiStatusStr(WiFi.status());
+    Serial.print(statusStr);
     blink();
   }
   Serial.println("");
@@ -40,4 +43,39 @@ void blink() {
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+}
+
+// WL definitions from https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/include/wl_definitions.h
+/*
+    WL_NO_SHIELD        = 255,   // for compatibility with WiFi Shield library
+    WL_IDLE_STATUS      = 0,
+    WL_NO_SSID_AVAIL    = 1,
+    WL_SCAN_COMPLETED   = 2,
+    WL_CONNECTED        = 3,
+    WL_CONNECT_FAILED   = 4,
+    WL_CONNECTION_LOST  = 5,
+    WL_DISCONNECTED     = 6
+*/
+char *wifiStatusStr(int statusCode) {
+  switch(statusCode) {
+    case 255:
+      return "WL_NO_SHIELD";
+    case 0:
+      return "WL_IDLE_STATUS";
+    case 1:
+      return "WL_NO_SSID_AVAIL";
+    case 2:
+      return "WL_SCAN_COMPLETED";
+    case 3:
+      return "WL_CONNECTED";
+    case 4:
+      return "WL_CONNECT_FAILED";
+    case 5:
+      return "WL_CONNECTION_LOST";
+    case 6:
+      return "WL_DISCONNECTED";
+  }
+  char statusStr[2];
+  itoa(statusCode, statusStr, 10);
+  return statusStr;
 }

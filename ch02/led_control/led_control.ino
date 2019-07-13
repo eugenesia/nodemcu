3c0cd4426b87c73df37315afdbf37a52fbca9c66
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-14 00:30:33 +0100

    Add led_control

---
+++ ch02/led_control/led_control.ino
@@ -0,0 +1,15 @@
+// Import required libraries
+#include <ESP8266WiFi.h>
+
+void setup() {  
+
+  // Set GPIO 5 as output
+  pinMode(5, OUTPUT);
+
+  // Set GPIO 5 on a HIGH state
+  digitalWrite(5, HIGH);
+
+}
+void loop() {
+
+}

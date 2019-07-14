b58e8760d5b0b8a094f138cfbf1ff7fab0811de4
Eugene Sia <eugene@eugenesia.co.uk>
2019-07-14 01:07:38 +0100

    Use pin D5 instead of 5 for NodeMCU

--- ch02/led_control/led_control.ino
+++ ch02/led_control/led_control.ino
@@ -1,15 +1,17 @@
 // Import required libraries
 #include <ESP8266WiFi.h>
 
-void setup() {  
+void setup() {
 
-  // Set GPIO 5 as output
-  pinMode(5, OUTPUT);
-
-  // Set GPIO 5 on a HIGH state
-  digitalWrite(5, HIGH);
-
+  // Set GPIO D5 as output
+  // NodeMCU needs pin targeted using D5 instead of 5
+  // See https://tttapa.github.io/ESP8266/Chap04%20-%20Microcontroller.html
+  pinMode(D5, OUTPUT);
 }
 void loop() {
+  digitalWrite(D5, LOW);
+  delay(1000);
 
+  digitalWrite(D5, HIGH);
+  delay(1000);
 }
